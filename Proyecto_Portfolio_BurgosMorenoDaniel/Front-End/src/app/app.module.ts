719df66b21356c238ee174b47a52b13011db8354
Kchuwaka <106410455+DaBuMo@users.noreply.github.com>
2023-04-07 12:08:26 -0300

    Organizacion codigo html, creacion del recuadro de proyecto y finalizacion front

---
+++ Proyecto_Portfolio_BurgosMorenoDaniel/Front-End/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FotoPerfilComponent } from './components/foto-perfil/foto-perfil.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ListaEmpleosComponent } from './components/lista-empleos/lista-empleos.component';
+import { DescripcionExperienciasComponent } from './components/descripcion-experiencias/descripcion-experiencias.component';
+import { EstudiosComponent } from './components/estudios/estudios.component';
+import { DescripcionEstudiosComponent } from './components/descripcion-estudios/descripcion-estudios.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HardAndSoftComponent } from './components/hard-and-soft/hard-and-soft.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedesComponent,
+    BannerComponent,
+    FotoPerfilComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    ListaEmpleosComponent,
+    DescripcionExperienciasComponent,
+    EstudiosComponent,
+    DescripcionEstudiosComponent,
+    HardAndSoftComponent,
+    ProyectosComponent,
+    FooterComponent,
+    LoginComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
