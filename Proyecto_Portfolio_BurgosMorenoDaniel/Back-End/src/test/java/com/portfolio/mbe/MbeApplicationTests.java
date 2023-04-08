fde6da3cc5bcbb03c7cc60ae1d3c733a3fc5ce17
Kchuwaka <106410455+DaBuMo@users.noreply.github.com>
2023-04-08 13:12:38 -0300

    Implementacion de spring

---
+++ Proyecto_Portfolio_BurgosMorenoDaniel/Back-End/src/test/java/com/portfolio/mbe/MbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
